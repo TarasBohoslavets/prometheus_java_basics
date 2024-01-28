e548497e151f18de42649222d2fd242e40820cb0
Taras Bohoslavets <bogoslavetstaras@gmail.com>
2024-01-28 07:54:18 +0200

    ShellSort

---
+++ 01/ShellSort/src/ShellSort.java
@@ -0,0 +1,25 @@
+public class ShellSort {
+    public static void main ( String[] args ) {
+        int[] array = {30, 2, 10, 4, 6};
+        int length = array.length;
+
+        int step = length / 2;
+        while (step > 0) {
+            for (int i = 0; i < (length - step); i++) {
+                int j = i;
+                while (j >= 0 && array[j] > array[j + step]) {
+
+                    int temp = array[j];
+                    array[j] = array[j + step];
+                    array[j + step] = temp;
+                    j--;
+                }
+            }
+            step = step / 2;
+        }
+
+        for (int i = 0; i < length; i++) {
+            System.out.print ( array[i] + " " );
+        }
+    }
+}
